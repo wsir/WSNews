68149dffd6159e363d45f11e82433a5df54037dd
wangsai <wangsai@mia.com>
2018-11-08 14:57:59 +0800

    create a repo

---
+++ WSNews/Classes/Core/WSNewsFile.h
@@ -0,0 +1,16 @@
+//
+//  WSNewsFile.h
+//  Pods-WSNews_Example
+//
+//  Created by WangSai on 2018/11/8.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WSNewsFile : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WSNews/Classes/Core/WSNewsFile.m
@@ -0,0 +1,12 @@
+//
+//  WSNewsFile.m
+//  Pods-WSNews_Example
+//
+//  Created by WangSai on 2018/11/8.
+//
+
+#import "WSNewsFile.h"
+
+@implementation WSNewsFile
+
+@end
