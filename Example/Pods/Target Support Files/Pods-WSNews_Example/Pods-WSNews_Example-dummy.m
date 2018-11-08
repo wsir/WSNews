3581888a6bf9d67ec6fd24147f79d97a4f15ee3d
wangsai <wangsai@mia.com>
2018-11-08 15:04:16 +0800

    greate

---
+++ Example/Pods/Target Support Files/Pods-WSNews_Example/Pods-WSNews_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WSNews_Example : NSObject
+@end
+@implementation PodsDummy_Pods_WSNews_Example
+@end
---
+++ Example/Pods/Target Support Files/WSNews/WSNews-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WSNews : NSObject
+@end
+@implementation PodsDummy_WSNews
+@end
